b50d486c7d02029a1d5ac416a66e4de4e02e12f0
akhlakm <me@akhlakm.com>
2022-09-13 09:44:32 -0400

    initial commit

---
+++ 1DownloadCSV.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -o us-census.csv https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv
+curl -O https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv
