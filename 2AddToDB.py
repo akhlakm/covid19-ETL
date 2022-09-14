e5516645a4c10acd064e825d135f8e89b44dd996
Akhlak Mahmood <109297014+akhlakm@users.noreply.github.com>
2022-09-13 20:21:54 -0400

    create index while adding data csv

--- 2AddToDB.py
+++ 2AddToDB.py
@@ -4,11 +4,11 @@ import sqlite3
 engine = sqlite3.connect('covid19.sqlite.db')
 
 counties = pd.read_csv("us-counties.csv")
-counties.to_sql('counties', con=engine, index=False, if_exists='replace')
+counties.to_sql('counties', con=engine, index=True, index_label='id', if_exists='replace')
 print(counties.head())
 
 census = pd.read_csv("us-census.csv", encoding = "ISO-8859-1")
-census.to_sql('census', con=engine, index=False, if_exists='replace')
+census.to_sql('census', con=engine, index=True, index_label='id', if_exists='replace')
 print(census.head())
 
 print("Done")
