f0116097365319111cd6d529bedbab99e0b05c56
Akhlak Mahmood <109297014+akhlakm@users.noreply.github.com>
2022-09-13 22:01:27 -0400

    add auditing script

---
+++ 4AuditData.py
@@ -0,0 +1,76 @@
+#%%
+import sqlite3
+import pandas as pd
+
+engine = sqlite3.connect('covid19.sqlite.db')
+
+#%% Schema
+def create_audit(conn):
+    try:
+        conn.execute("DROP TABLE audit;")
+    except sqlite3.OperationalError:
+        pass
+
+    sql = '''
+    CREATE TABLE audit (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tblname TEXT NOT NULL,
+        column  TEXT NOT NULL,
+        audit   TEXT NOT NULL,
+        rowval  TEXT NOT NULL,
+        date    DATE DEFAULT CURRENT_TIMESTAMP
+    );
+    '''
+    print(sql)
+    conn.execute(sql)
+    conn.commit()
+
+def null_rows(conn, table, column):
+    sql = f'''
+    INSERT INTO audit ('rowval', 'tblname', 'column', 'audit')
+    SELECT id, '{table}', '{column}', 'null'
+    FROM {table} WHERE {column} IS NULL;
+    '''
+    print(sql)
+    conn.execute(sql)
+
+    sql = f'''
+    INSERT INTO audit ('rowval', 'tblname', 'column', 'audit')
+    SELECT id, '{table}', '{column}', 'empty'
+    FROM {table} WHERE {column} = '';
+    '''
+    print(sql)
+    conn.execute(sql)
+
+def unique_values(conn, table, column):
+    sql = f'''
+    INSERT INTO audit ('rowval', 'tblname', 'column', 'audit')
+    SELECT DISTINCT({column}) as rowval, '{table}', '{column}', 'unique'
+    FROM {table} WHERE {column} IS NOT NULL;
+    '''
+    print(sql)
+    conn.execute(sql)
+
+
+create_audit(engine)
+
+for tabl in ['counties', 'census']:
+    cols = pd.read_sql(f"""
+        SELECT value
+        FROM profile_summary
+        WHERE tblname = '{tabl}' AND column = 'all' AND info = 'col_names'
+    """, engine).iloc[0,0].split(",")
+
+    dtypes = pd.read_sql(f"""
+        SELECT value
+        FROM profile_summary
+        WHERE tblname = '{tabl}' AND column = 'all' AND info = 'col_types'
+    """, engine).iloc[0,0].split(",")
+
+    for col, dt in zip(cols, dtypes):
+        print(tabl, col, dt)
+        null_rows(engine, tabl, col)
+        if dt == 'object':
+            unique_values(engine, tabl, col)
+
+engine.commit()
