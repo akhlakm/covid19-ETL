563628d08182ca5ceddf94b19513dab8d29ccb60
Akhlak Mahmood <109297014+akhlakm@users.noreply.github.com>
2022-09-13 21:07:10 -0400

    add id to profile_summary table

--- 3ProfileData.py
+++ 3ProfileData.py
@@ -6,16 +6,30 @@ engine = sqlite3.connect('covid19.sqlite.db')
 
 #%% Schema
 def create_profile(conn):
-    schema = ['table', 'column', 'info', 'value', 'profilev']
-    df = pd.DataFrame(columns = schema)
-    df = df.astype({'profilev': str})
-    df.to_sql('profile_summary', con=conn, if_exists='replace', index=False)
+    try:
+        conn.execute("DROP TABLE profile_summary;")
+    except sqlite3.OperationalError:
+        pass
+
+    sql = '''
+    CREATE TABLE profile_summary (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tblname TEXT NOT NULL,
+        column  TEXT NOT NULL,
+        info    TEXT NOT NULL,
+        value   TEXT NOT NULL,
+        date    DATE DEFAULT CURRENT_TIMESTAMP
+    );
+    '''
+    print(sql)
+    conn.execute(sql)
+    conn.commit()
 
 #%% Profile
 def profile(table, col, info, value):
     sql = ('INSERT INTO profile_summary'
-        f'("table", "column", "info", "value", "profilev")'
-        f'VALUES ("{table}", "{col}", "{info}", "{value}", DATE("now"));'
+        f'("tblname", "column", "info", "value") '
+        f'VALUES("{table}", "{col}", "{info}", "{value}");'
     )
     print(sql)
     engine.execute(sql)
@@ -50,10 +64,10 @@ def profile_columns(table):
         sqls.append(f'(SELECT COUNT({col}) FROM {table} WHERE {col} IS NULL) as {col}_nullCount')
         sqls.append(f'(SELECT MIN({col}) FROM {table} WHERE {col} IS NOT NULL) as {col}_min')
         sqls.append(f'(SELECT MAX({col}) FROM {table} WHERE {col} IS NOT NULL) as {col}_max')
-        if dt == object:
-            sqls.append(f'(SELECT COUNT(DISTINCT({col})) FROM {table} WHERE {col} IS NOT NULL) as {col}_uniqueCount')
-        elif dt == float:
+        if dt == float:
             sqls.append(f'(SELECT AVG({col}) FROM {table} WHERE {col} IS NOT NULL) as {col}_avg')
+        elif dt == object:
+            sqls.append(f'(SELECT COUNT(DISTINCT({col})) FROM {table} WHERE {col} IS NOT NULL) as {col}_uniqueCount')
 
     sql = "SELECT" + ", ".join(sqls) + ";"
     df = pd.read_sql(sql, engine)
@@ -62,50 +76,21 @@ def profile_columns(table):
         profile(table, col, 'null_count', df[col+'_nullCount'][0])
         profile(table, col, 'min', df[col+'_min'][0])
         profile(table, col, 'max', df[col+'_max'][0])
-        if dt == object: profile(table, col, 'unique_count', df[col+'_uniqueCount'][0])
-        elif dt == float: profile(table, col, 'avg', df[col+'_avg'][0])
-
-def profile_numeric_columns(table):
-    columns = col_names(table)
-    dtypes = data_types(table)
-
-    for col, dt in zip(columns, dtypes):
-        if dt == float:
-            sql = f'''
-            SELECT
-                MIN({col}) AS col_min, 
-                MAX({col}) AS col_max, 
-                AVG({col}) AS col_avg
-            FROM {table}
-            WHERE {col} IS NOT NULL;
-            '''
-
-        elif dt == int or dt == str or dt == object:
-            sql = f'''
-            SELECT
-                MIN({col}) AS col_min, 
-                MAX({col}) AS col_max
-            FROM {table}
-            WHERE {col} IS NOT NULL;
-            '''
-
-        df = pd.read_sql(sql, engine)
-        for icol in df.columns:
-            profile(table, col, icol, df[icol][0])
+        if dt == float: profile(table, col, 'avg', df[col+'_avg'][0])
+        elif dt == object: profile(table, col, 'unique_count', df[col+'_uniqueCount'][0])
 
 #%%
+
 create_profile(engine)
 
-profile_row_counts('census')
-profile_row_counts('counties')
-profile_col_count('census')
-profile_col_count('counties')
-profile_col_names('census')
-profile_col_names('counties')
-profile_numeric_columns('census')
-profile_numeric_columns('counties')
-#%%
+for tabl in ['counties', 'census']:
+    profile_row_counts(tabl)
+    profile_col_count(tabl)
+    profile_col_names(tabl)
+    profile_data_types(tabl)
+    profile_columns(tabl)
 
+#%%
 engine.commit()
 
 # %%
